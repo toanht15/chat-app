f1ec6e4f53e4337d706865c8849ef6e8927dcb6a
rena.hidaka <rena.hidaka@medialink-ml.co.jp>
2016-01-06 17:42:47 +0900

    Upgrade: add ng-change func to ngFormHelper

--- contact_site/app/View/Helper/ngFormHelper.php
+++ contact_site/app/View/Helper/ngFormHelper.php
@@ -12,11 +12,18 @@ class ngFormHelper extends AppHelper {
     public function input($fieldName, $options = [], $ngOptions = []) {
         $value = null;
         $this->setEntity($fieldName);
-        if ( !empty($ngOptions) && !empty($ngOptions['entity']) ) {
-            $value = Hash::get($this->request->data, $ngOptions['entity']);
+        if ( !empty($ngOptions) ) {
+            // set: model && add key-value
+            if ( !empty($ngOptions['entity']) ) {
+                $value = Hash::get($this->request->data, $ngOptions['entity']);
+                $options['ng-model'] = $fieldName;
+                $options['ng-init'] = $fieldName .'="' . $value . '";';
+            }
+            // set: ng-change
+            if ( !empty($ngOptions['change']) ) {
+                $options['ng-change'] = $ngOptions['change'];
+            }
         }
-        $options['ng-model'] = $fieldName;
-        $options['ng-init'] = $fieldName .'="' . $value . '";';
         return $this->Form->input($fieldName, $options);
     }
 }
