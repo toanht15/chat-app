d2e899af757ac952cf4b58980c2dd89294aa5e51
rena.hidaka <rena.hidaka@medialink-ml.co.jp>
2016-01-20 10:09:58 +0900

    Revert: Correct the socket server's port

--- contact_site/app/Config/const.php
+++ contact_site/app/Config/const.php
@@ -1,7 +1,7 @@
 <?php
 /* 定数定義 */
 // Nodeサーバー
-define('C_NODE_SERVER_ADDR', "//socket.localhost:8080");
+define('C_NODE_SERVER_ADDR', "//socket.localhost:9090");
 
 // 表示設定種別
 define('C_WIDGET_DISPLAY_CODE_SHOW', 1); // 常に表示する
