fe9ffb01188c7e79ed839c1dde231d3cd5a474be
rena.hidaka <rena.hidaka@medialink-ml.co.jp>
2016-07-04 22:08:12 +0900

    Hotfix: ローディング画像のハードコードを削除

--- compiler/socketJs/websocket/common.js
+++ compiler/socketJs/websocket/common.js
@@ -394,7 +394,7 @@ var socket, // socket.io
         div.id = this.id;
         div.style.cssText = "position: fixed; top: 0; left: 0; bottom: 0; right: 0; background-color: rgba(68,68,68,0.5); z-index: 99999";
         var loadImg = document.createElement("img");
-        loadImg.src = "http://socket.localhost:8080/img/loading.gif";
+        loadImg.src = window.info.site.files + "/img/loading.gif";
         loadImg.alt = "画面遷移中";
         loadImg.style.cssText = "position: fixed; top: 50%; left: 50%; margin-left: -50px; margin-top: -50px";
         div.appendChild(loadImg);
