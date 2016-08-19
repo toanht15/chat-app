105a219c3112550f42c3bb5599913c6f9be92b03
rena.hidaka <rena.hidaka@medialink-ml.co.jp>
2016-08-19 09:18:15 +0900

    Update: 【ウィジェット】Safariでの表示崩れ対応（flex-box）

--- compiler/socketJs/websocket/common.js
+++ compiler/socketJs/websocket/common.js
@@ -198,7 +198,7 @@ var socket, // socket.io
       html += '      #sincloBox[data-openflg="false"] p#widgetTitle:after { transform: rotate(180deg); }';
       html += '      #sincloBox > section { background-color: #FFF; border-top: none; }';
       html += '      #sincloBox ul#chatTalk li a, #sincloBox #fotter a {  text-decoration: underline; }';
-      html += '      #sincloBox section { flex-direction: column; -ms-flex-direction: column }';
+      html += '      #sincloBox section { flex-direction: column; -ms-flex-direction: column; -webkit-flex-direction: flex }';
 
       // チャットを使用する際
       if ( window.info.contract.chat ) {
@@ -359,10 +359,10 @@ html += ' #sincloBox ul sinclo-chat { clear: both!important } ';
           html += '      #sincloBox section#callTab #telIcon { color: ' + widget.mainColor + '; display: block; width: 50px; height: 50px; float: left; background-color: #3EA3DE; border-radius: 25px; padding: 3px }';
           html += '      #sincloBox section#callTab #telContent { display: block; overflow-y: auto; overflow-x: hidden; max-height: 119px }';
           if ( window.info.contract.chat ) {
-            html += '      #sincloBox section#callTab #telContent .tblBlock {  text-align: center;  margin: 0 auto;  width: 240px;  display: table;  flex-direction: column; -ms-flex-direction: column;  align-content: center;  height: 119px!important;  justify-content: center; }';
+            html += '      #sincloBox section#callTab #telContent .tblBlock {  text-align: center;  margin: 0 auto;  width: 240px;  display: table;  flex-direction: column; -ms-flex-direction: column; -webkit-flex-direction: flex;  align-content: center;  height: 119px!important;  justify-content: center; }';
           }
           else {
-            html += '      #sincloBox section#callTab #telContent .tblBlock { text-align: center; margin: 0 auto; width: 240px; display: table; flex-direction: column; -ms-flex-direction: column; align-content: center; justify-content: center; overflow-x: hidden; overflow-y: auto }';
+            html += '      #sincloBox section#callTab #telContent .tblBlock { text-align: center; margin: 0 auto; width: 240px; display: table; flex-direction: column; -ms-flex-direction: column; -webkit-flex-direction: flex; align-content: center; justify-content: center; overflow-x: hidden; overflow-y: auto }';
           }
           html += '      #sincloBox section#callTab #telContent span { word-wrap: break-word ;word-break: break-all; font-size: 11px; line-height: 1.5!important; color: #6B6B6B; white-space: pre-wrap; max-height: 119px; display: table-cell; vertical-align: middle; text-align: center }';
           html += '      #sincloBox section#callTab #accessIdArea { height: 50px; display: block; margin: 10px auto; width: 80%; padding: 7px;  color: #FFF; background-color: rgb(188, 188, 188); font-size: 25px; font-weight: bold; text-align: center; border-radius: 15px } ';
@@ -547,14 +547,12 @@ html += ' #sincloBox ul sinclo-chat { clear: both!important } ';
             $(this).addClass("selected");
 
             if ( clickTab === "call" ) {
-              $("#callTab").css('display', 'flex');
-              $("#callTab").css('display', '-ms-flexbox');
+              $("#callTab").css('display', 'flex').css('display', '-ms-flexbox').css('display', '-webkit-flex');
               $("#chatTab").hide();
             }
             else {
               $("#callTab").hide();
-              $("#chatTab").css('display', 'flex');
-              $("#chatTab").css('display', '-ms-flexbox');
+              $("#chatTab").css('display', 'flex').css('display', '-ms-flexbox').css('display', '-webkit-flex');
               sinclo.chatApi.showUnreadCnt();
               sinclo.chatApi.scDown();
             }
