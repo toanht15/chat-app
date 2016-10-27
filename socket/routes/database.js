95d1f5ddd5cd9064ae4f39987f7a99ea6e619057
= <yuki.henmi@medialink-ml.co.jp>
2016-10-27 20:01:51 +0900

    Delete: database設定ファイルの削除

--- socket/routes/database.js
+++
@@ -1,6 +0,0 @@
-/* DataBase Settings */
-process.env.DB_HOST = 'localhost';
-process.env.DB_USER = 'root';
-process.env.DB_PASS = 'password';
-process.env.DB_NAME = 'sinclo_db';
-process.env.WS_PORT = '9090';
