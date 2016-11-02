9527c45ee19472b37ab8a40b45daccbec865b19a
yuki.henmi <yuki.henmi@medialink-ml.co.jp>
2016-11-02 19:16:32 +0900

    admin_site ログインコントローラー作成前

---
+++ contact_site/app/Config/Migration/1478080163_create_table_administrators.php
@@ -0,0 +1,42 @@
+<?php
+class CreateTableAdministrators extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'create_table_administrators';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(
+		),
+		'down' => array(
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		return true;
+	}
+}
