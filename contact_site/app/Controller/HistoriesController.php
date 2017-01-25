8f15ce3537b275a2150fa06ac114bc5545c8a1a0
yuki.henmi <yuki.henmi@medialink-ml.co.jp>
2017-01-25 17:31:23 +0900

    Upgrade: 【履歴】　検索絞り込み　全検索のみ開いたときに色を変える 挙げ直し

--- contact_site/app/Controller/HistoriesController.php
+++ contact_site/app/Controller/HistoriesController.php
@@ -503,7 +503,6 @@ class HistoriesController extends AppController {
 
     //履歴検索機能
     if($this->request->is('post')) {
-      $this->log($this->request->data,LOG_DEBUG);
       $this->Session->write('Thistory', $this->data);
     }
 
