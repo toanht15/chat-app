840e5a5bb093f7f4fac9790f4afb7a47bb83f562
yuki.henmi <yuki.henmi@medialink-ml.co.jp>
2017-06-16 11:59:03 +0900

    Upgrade:【統計】migration table名変更

---
+++ contact_site/app/Config/Migration/1496989656_create_table_t_conversation_count.php
@@ -0,0 +1,54 @@
+<?php
+class CreateTableTConversationCount extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'create_table_t_conversation_count';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(
+			'create_table' => array(
+				't_conversation_count' => array(
+					'visitors_id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 20, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'conversation_count' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+					'indexes' => array(
+					),
+					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
+				),
+			),
+		),
+		'down' => array(
+			'drop_table' => array(
+				't_conversation_count'
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		return true;
+	}
+}
