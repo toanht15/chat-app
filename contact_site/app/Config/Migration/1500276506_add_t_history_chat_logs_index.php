70f30a05f4166f38be9de80cdfa4164731fcec3b
yuki.henmi <yuki.henmi@medialink-ml.co.jp>
2017-07-18 10:15:17 +0900

    Upgrade:【統計】マイグレーションファイル　createTable削り

--- contact_site/app/Config/Migration/1500276506_add_t_history_chat_logs_index.php
+++ contact_site/app/Config/Migration/1500276506_add_t_history_chat_logs_index.php
@@ -15,50 +15,6 @@ class AddIndex extends CakeMigration {
  */
 	public $migration = array(
 		'up' => array(
-			'create_table' => array(
-				't_conversation_count' => array(
-					'visitors_id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 20, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'conversation_count' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
-					'indexes' => array(
-					),
-					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
-				),
-				't_history_chat_active_users' => array(
-					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
-					't_history_chat_logs_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
-					'm_companies_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
-					'm_users_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
-					'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
-					'indexes' => array(
-						'PRIMARY' => array('column' => 'id', 'unique' => 1),
-					),
-					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
-				),
-				't_history_widget_displays' => array(
-					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
-					'm_companies_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'index'),
-					'tab_id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'created' => array('type' => 'datetime', 'null' => true, 'default' => null, 'key' => 'index'),
-					'indexes' => array(
-						'PRIMARY' => array('column' => 'id', 'unique' => 1),
-						'idx_t_history_widget_displays_created' => array('column' => 'created', 'unique' => 0),
-						'idx_t_history_widget_displays_m_companies_id' => array('column' => 'm_companies_id', 'unique' => 0),
-					),
-					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
-				),
-				't_logins' => array(
-					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
-					'm_companies_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
-					'm_users_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
-					'ip_address' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 15, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'user_agent' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 300, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
-					'indexes' => array(
-						'PRIMARY' => array('column' => 'id', 'unique' => 1),
-					),
-					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
-				),
-			),
 			'create_field' => array(
 				't_history_chat_logs' => array(
 					'message_distinction' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'after' => 'message_type'),
@@ -82,9 +38,6 @@ class AddIndex extends CakeMigration {
 			),
 		),
 		'down' => array(
-			'drop_table' => array(
-				't_conversation_count', 't_history_chat_active_users', 't_history_widget_displays', 't_logins'
-			),
 			'drop_field' => array(
 				't_history_chat_logs' => array('message_distinction', 'message_request_flg', 'indexes' => array('idx_t_history_chat_logs_t_histories_id', 'idx_t_history_chat_logs_message_type', 'idx_t_history_chat_logs_message_request_flg', 'idx_t_history_chat_logs_achievement_flg')),
 			),
