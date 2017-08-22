018e81370fbb3c3ad1d44113d6bb7169f1b77fcf
yuki.henmi <yuki.henmi@medialink-ml.co.jp>
2017-08-22 21:00:32 +0900

    Upgrade:【統計】マイグレーションファイル変更

--- contact_site/app/Config/Migration/1503371673_add_t_history_chat_logs_index.php
+++ contact_site/app/Config/Migration/1503371673_add_t_history_chat_logs_index.php
@@ -15,14 +15,6 @@ class AddTHistoryChatLogsIndex extends CakeMigration {
  */
 	public $migration = array(
 		'up' => array(
-			'alter_field' => array(
-				't_history_chat_logs' => array(
-					'created' => array('type' => 'datetime', 'null' => false, 'default' => '0000-00-00 00:00:00.00', 'length' => null, 'key' => 'primary'),
-				),
-			),
-			'drop_field' => array(
-				't_history_chat_logs' => array('indexes' => array('PRIMARY')),
-			),
 			'create_field' => array(
 				't_history_chat_logs' => array(
 					'indexes' => array(
@@ -35,18 +27,6 @@ class AddTHistoryChatLogsIndex extends CakeMigration {
 			),
 		),
 		'down' => array(
-			'alter_field' => array(
-				't_history_chat_logs' => array(
-					'created' => array('type' => 'datetime', 'null' => true, 'default' => null, 'length' => 2),
-				),
-			),
-			'create_field' => array(
-				't_history_chat_logs' => array(
-					'indexes' => array(
-						'PRIMARY' => array(),
-					),
-				),
-			),
 			'drop_field' => array(
 				't_history_chat_logs' => array('indexes' => array('idx_t_history_chat_logs_message_type_t_histories_id_m_users_id', 'idx_t_history_chat_logs_achievement_flg_t_histories_id_users_id', 'idx_t_history_chat_logs_message_request_flg_t_histories_id', 'PRIMARY')),
 			),
