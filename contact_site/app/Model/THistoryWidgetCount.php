f74c0b516d02b4a3a9024b8192e6b8c246c8cbff
yuki.henmi <yuki.henmi@medialink-ml.co.jp>
2017-09-12 19:52:29 +0900

    Upgrade:【統計】テーブル追加

---
+++ contact_site/app/Model/THistoryWidgetCount.php
@@ -0,0 +1,12 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * THistoryWidgetCount Model
+ *ウィジェット件数集計テーブル
+ *
+ */
+class THistoryWidgetCount extends AppModel {
+
+  public $name = "THistoryWidgetCount";
+
+}
