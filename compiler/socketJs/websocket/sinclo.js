6da9eaf333fc2c24d0d20de8b837d4f39a817bee
masashi.shimizu <masashi.shimizu@medialink-ml.co.jp>
2017-10-04 11:59:00 +0900

    Update : 画面キャプチャ共有時にウィジェットは表示しない

--- compiler/socketJs/websocket/sinclo.js
+++ compiler/socketJs/websocket/sinclo.js
@@ -479,6 +479,10 @@
       var title = location.host + 'の内容';
       var content = location.host + 'が閲覧ページへのアクセスを求めています。<br>許可しますか';
       popup.ok = function() {
+        var sincloBox = document.getElementById("sincloBox");
+        if( sincloBox ){
+          sincloBox.style.display = "none";
+        }
         userInfo.coBrowseConnectToken = obj.connectToken;
         storage.s.set('coBrowseConnectToken', obj.connectToken);
         var params = {
