ea760e9b2bb02f5bff349461abf3431b307464c7
shingo.sato <shingo.sato@medialink-ml.co.jp>
2017-10-06 20:00:34 +0900

    「指摘事項修正」 ・スマホ実機横画面でバナーの文言が上下ずれを起こす現象を修正

--- compiler/socketJs/websocket/common.js
+++ compiler/socketJs/websocket/common.js
@@ -931,7 +931,7 @@ var socket, // socket.io
         }
         else{
           //横
-          var paddingpx = 'padding: '+ 10 +'px 0';
+          var paddingpx = 'line-height: '+ 77 +'px';
         }
       }
       else{
