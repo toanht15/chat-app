6a3d291b2b296d7a028dcd150513682fc4e200d2
shingo.sato <shingo.sato@medialink-ml.co.jp>
2017-10-23 15:38:20 +0900

    「実装漏れ分修正」 ・t_campaignsテーブルsortカラム追加マイグレーションファイル

---
+++ contact_site/app/Config/Migration/1508740428_add_column_sort_t_campaigns.php
@@ -0,0 +1,50 @@
+<?php
+class AddColumnSortTCampaignsCakeMigration {
+
+  /**
+   * Migration description
+   *
+   * @var string
+   */
+  public $description = 'add_column_sort_t_campaigns';
+
+  /**
+   * Actions to be performed
+   *
+   * @var array $migration
+   */
+  public $migration = array(
+      'up' => array(
+          'create_field' => array(
+              't_campaigns' => array(
+                  'sort' => array('type' => 'integer', 'null' => true, 'default' => '999', 'unsigned' => false, 'comment' => 'ソート順', 'after' => 'comment'),
+              )
+          )
+      ),
+      'down' => array(
+          'drop_field' => array(
+              't_campaigns' => array('sort'),
+          )
+      )
+  );
+
+  /**
+   * Before migration callback
+   *
+   * @param string $direction Direction of migration process (up or down)
+   * @return bool Should process continue
+   */
+  public function before($direction) {
+    return true;
+  }
+
+  /**
+   * After migration callback
+   *
+   * @param string $direction Direction of migration process (up or down)
+   * @return bool Should process continue
+   */
+  public function after($direction) {
+    return true;
+  }
+}
