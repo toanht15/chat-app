c57f988575ca08223ea10b4ae5f29671b77e6214
masashi.shimizu <masashi.shimizu@medialink-ml.co.jp>
2017-11-07 13:16:32 +0900

    HotFix : 自動返信後にチャット対応中のフラグがtrueとなってしまうバグを修正

--- compiler/socketJs/websocket/sinclo.js
+++ compiler/socketJs/websocket/sinclo.js
@@ -1808,10 +1808,10 @@
                     setTimeout(function(){
                       sinclo.trigger.setAction(message.id, message.action_type, message.activity);
                       sinclo.trigger.processing = false;
-                      if(conditionKey === 7) {
-                        // 自動返信実行後はチャット中のフラグを立てる
-                        storage.s.set('chatAct','true');
-                      }
+                      // if(conditionKey === 7) {
+                      //   // 自動返信実行後はチャット中のフラグを立てる
+                      //   storage.s.set('chatAct','true');
+                      // }
                     }, ret);
                 } else if(ret && typeof(ret) === 'object') {
                     sinclo.trigger.timerTriggeredList[message.id] = false;
@@ -1865,10 +1865,10 @@
                         }
                       }
                       sinclo.trigger.setAction(message.id, message.action_type, message.activity);
-                      if(conditionKey === 7) {
-                        // 自動返信実行後はチャット中のフラグを立てる
-                        storage.s.set('chatAct','true');
-                      }
+                      // if(conditionKey === 7) {
+                      //   // 自動返信実行後はチャット中のフラグを立てる
+                      //   storage.s.set('chatAct','true');
+                      // }
                       sinclo.trigger.processing = false;
                     }, ret);
                 }
