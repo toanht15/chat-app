550375d5337afaad63099dd17d72f280684a61c9
masashi.shimizu <masashi.shimizu@medialink-ml.co.jp>
2017-11-14 20:53:39 +0900

    Delete : 間違ってコミットしてしまった当該ファイルを削除

--- admin_site/app/Config/myConst.php
+++
@@ -1,6 +0,0 @@
-<?php
-$config = [];
-
-define('C_NODE_SERVER_ADDR', "//node.sinclo.local"); // Nodeサーバー
-define('C_NODE_SERVER_FILE_PORT', ""); // NodeサーバーのHTTP通信ポート
-define('C_NODE_SERVER_WS_PORT', ""); // WS通信ポート
