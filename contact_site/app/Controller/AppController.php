1bf1e8bfe7258d78cd4e41b4ffcebd401751442b
masashi.shimizu <masashi.shimizu@medialink-ml.co.jp>
2017-11-17 15:45:11 +0900

    Merge remote-tracking branch 'remotes/origin/integrate_landscape' into development
    # Conflicts:
    #	contact_site/app/Config/const.php
    #	contact_site/app/Controller/AppController.php

--- contact_site/app/Controller/AppController.php
+++ contact_site/app/Controller/AppController.php
@@ -69,7 +69,7 @@ class AppController extends Controller {
     C_COMPANY_USE_DICTIONARY_CATEGORY => false, // 定型文カテゴリ
     C_COMPANY_USE_LA_CO_BROWSE => false, // 画面キャプチャ共有
     C_COMPANY_USE_HIDE_REALTIME_MONITOR => false, // 通常時リアルタイムモニタ非表示
-    C_COMPANY_USE_OPERATING_HOUR => false //営業時間設定
+    C_COMPANY_USE_OPERATING_HOUR => false, //営業時間設定
     C_COMPANY_REF_COMPANY_DATA => false // 企業情報参照（Landscape）
   ];
 
