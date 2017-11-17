d845fd0e0476f04400485352f793ad1db4019f30
masashi.shimizu <masashi.shimizu@medialink-ml.co.jp>
2017-11-17 11:06:44 +0900

    Update : 重複回避のためsincloSessionIdをUUID化

--- socket/routes/module/socket_ctrl.js
+++ socket/routes/module/socket_ctrl.js
@@ -1,4 +1,5 @@
 var database = require('../database');
+var uuid = require('node-uuid');
 
 // mysql
 var mysql = require('mysql'),
@@ -1004,7 +1005,7 @@ io.sockets.on('connection', function (socket) {
     if ( res.type !== 'admin' ) {
       if ( data.userId === undefined || data.userId === '' || data.userId === null ) {
         send.userId = makeUserId();
-        send.sincloSessionId = send.userId;
+        send.sincloSessionId = uuid.v4();
       }
       if ( data.accessId === undefined || data.accessId === '' || data.accessId === null ) {
         send.accessId = ('000' + Math.floor(Math.random() * 10000)).slice(-4);
