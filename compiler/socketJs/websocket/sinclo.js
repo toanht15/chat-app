b2adfbd9a83c98cd8fd835aff9ce020d99382bb5
yuki.henmi <yuki.henmi@medialink-ml.co.jp>
2017-11-27 14:15:01 +0900

    fix:【営業時間設定】インデントズレ

--- compiler/socketJs/websocket/sinclo.js
+++ compiler/socketJs/websocket/sinclo.js
@@ -1380,7 +1380,6 @@
         },
         autoMessages: {
           push: function(id, obj) {
-
             var list = this.get(true);
             if(!this.exists(id)) {
               list[id] = obj;
@@ -1429,7 +1428,6 @@
             // 論理的にフラグを付ける
             var list = this.get();
             Object.keys(list).forEach(function(id, index, arr) {
-
               list[id]['applied'] = true;
             });
             storage.s.set('amsg', JSON.stringify(list));
@@ -2273,7 +2271,6 @@
                 isAutoSpeech: isSpeechContent
             };
 
-
             if(!sinclo.chatApi.autoMessages.exists(data.chatId) && !isSpeechContent) {
               //resAutoMessagesで表示判定をするためにidをkeyとして空Objectを入れる
               sinclo.chatApi.autoMessages.push(data.chatId, {});
@@ -2299,7 +2296,6 @@
             }
 
             if ( String(type) === "1" && ('message' in cond) && (String(chatActFlg) === "false") ) {
-
                 if(sinclo.chatApi.autoMessages.exists(id)){
                   console.log("exists id : " + id);
                   return;
