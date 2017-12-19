40fa38c450847831af510e7d4717fdabf03cbfe0
masashi.shimizu <masashi.shimizu@medialink-ml.co.jp>
2017-12-19 14:52:34 +0900

    Fix : 変数指定間違え

--- compiler/socketJs/websocket/sinclo.js
+++ compiler/socketJs/websocket/sinclo.js
@@ -427,7 +427,7 @@
 
       if(check.isset(storage.s.get('inactiveTimeout')) && storage.s.get('inactiveTimeout') === "true") {
         // 再接続扱いとする
-        data.inactiveReconnect = true;
+        emitData.inactiveReconnect = true;
       }
 
       emit('connected', {
