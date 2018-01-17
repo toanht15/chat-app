f06ace6012bf2df596ddfaf78baac4cae937d456
yuki.henmi <yuki.henmi@medialink-ml.co.jp>
2018-01-17 13:08:21 +0900

    Upgrade:【チャット履歴】バグ修正

--- socket/routes/module/socket_ctrl.js
+++ socket/routes/module/socket_ctrl.js
@@ -8,7 +8,7 @@ var mysql = require('mysql'),
       host: process.env.DB_HOST || 'localhost',
       user: process.env.DB_USER || 'root',
       password: process.env.DB_PASS || 'password',
-      database: process.env.DB_NAME || 'sinclo_db2'
+      database: process.env.DB_NAME || 'sinclo_db'
     });
 
 // log4js
