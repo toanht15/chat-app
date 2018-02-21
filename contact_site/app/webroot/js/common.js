8e6df52b39e7600e3f18adce60731de756255565
yuki.henmi <yuki.henmi@medialink-ml.co.jp>
2018-02-21 19:50:39 +0900

    消し忘れ

--- contact_site/app/webroot/js/common.js
+++ contact_site/app/webroot/js/common.js
@@ -30,17 +30,3 @@ var getData = function(elm, key){
     }
     return data;
 }
-
-//パスワード作成
-function random(){
-  var l = 8;
-  // 生成する文字列に含める文字セット
-  var c = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJILMNOPQRSTUVWXYZ0123456789";
-  var cl = c.length;
-  var data = "";
-  var r = "";
-  for(var i=0; i<l; i++){
-    r += c[Math.floor(Math.random()*cl)];
-  }
-  return r;
-}
