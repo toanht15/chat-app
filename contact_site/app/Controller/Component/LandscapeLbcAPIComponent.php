0e4654977d8dc540deaa0353b5f9019910f019c3
masashi.shimizu <masashi.shimizu@medialink-ml.co.jp>
2018-03-19 14:50:28 +0900

    Fix : IDE上でエラーが出続ける箇所を修正

--- contact_site/app/Controller/Component/LandscapeLbcAPIComponent.php
+++ contact_site/app/Controller/Component/LandscapeLbcAPIComponent.php
@@ -9,7 +9,7 @@ App::uses('LandscapeAPIComponent', 'Controller/Component');
 class LandscapeLbcAPIComponent extends LandscapeAPIComponent
 {
   const API_CALL_TIMEOUT = 5;
-  const LANDSCAPE_DATA_EXPIRE_SEC = 60 * 60 * 24 * 90; // 90日
+  const LANDSCAPE_DATA_EXPIRE_SEC = 7776000; // 90日
 
   const PATTERN_IP = "/^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$/";
   const PATTERN_LBC_CODE = "/^[0-9]+$/";
