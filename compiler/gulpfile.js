8ad6e50b5a66947002668ea923993e28176a1fbe
masashi.shimizu <masashi.shimizu@medialink-ml.co.jp>
2018-04-17 12:30:33 +0900

    Update : min.jsとgzが両方生成できるよう修正

--- compiler/gulpfile.js
+++ compiler/gulpfile.js
@@ -3,17 +3,23 @@ var gulp = require('gulp'),
   cssnext = require('gulp-cssnext'),
   jade = require('gulp-jade'),
   gzip = require('gulp-gzip'),
+  runSequence = require('run-sequence'),
   path = {
     css: '../contact_site/app/webroot/css/',
+    cssFile: '../contact_site/app/webroot/css/*.css',
     scss: './scss/',
     adCss: '../admin_site/app/webroot/css/',
+    adCssFile: '../admin_site/app/webroot/css/*.css',
     adScss: './admin_scss/',
     js: 'socketJs/websocket/*.js',
     minjs: '../socket/webroot/websocket/',
+    minjsFile: '../socket/webroot/websocket/*.min.js',
     jade: '../socket/views/*.jade',
     outOfJs: '../socket/webroot/',
+    outOfJsFile: '../socket/webroot/*.min.js',
     socketSass: '../socket/public/stylesheets/',
-    outOfCssToSocket: '../socket/webroot/css/'
+    outOfCssToSocket: '../socket/webroot/css/',
+    outOfCssToSocketFile: '../socket/webroot/css/*.css'
   };
 
 var uglify = require('gulp-uglify'),
@@ -35,7 +41,12 @@ gulp.task('admin-scss-compile', function(){
       console.log(err.message);
     })
     .pipe(cssnext())
-    .pipe(gzip({ gzipOptions: { level: 9 } }))
+    .pipe(gulp.dest(path.adCss));
+});
+
+gulp.task('compress-gzip-admin-css', function(){
+  return gulp.src(path.adCssFile)
+    .pipe(gzip({ gzipOptions: { level: 9 }, deleteMode: path.adCss }))
     .pipe(gulp.dest(path.adCss));
 });
 
@@ -46,10 +57,14 @@ gulp.task('contact-scss-compile', function(){
       console.log(err.message);
     })
     .pipe(cssnext())
-    .pipe(gzip({ gzipOptions: { level: 9 } }))
     .pipe(gulp.dest(path.css));
 });
 
+gulp.task('compress-gzip-contact-css', function(){
+  return gulp.src(path.cssFile)
+    .pipe(gzip({ gzipOptions: { level: 9 }, deleteMode: path.css }))
+    .pipe(gulp.dest(path.css));
+});
 
 gulp.task('socket-sass-compile', function(){
   return gulp.src(path.socketSass + '**/*.sass')
@@ -62,7 +77,13 @@ gulp.task('socket-sass-compile', function(){
     .pipe(gulp.dest(path.outOfCssToSocket));
 });
 
-gulp.task('js-minify', function(){
+gulp.task('compress-gzip-socket-css', function(){
+  return gulp.src(path.outOfCssToSocketFile)
+    .pipe(gzip({ gzipOptions: { level: 9 }, deleteMode: path.outOfCssToSocket }))
+    .pipe(gulp.dest(path.outOfCssToSocket));
+});
+
+gulp.task('minify-js', function(){
   return gulp.src(path.js)
     .pipe(uglify(uglifyOpt))
     .pipe(rename({
@@ -72,7 +93,7 @@ gulp.task('js-minify', function(){
     .pipe(gulp.dest(path.minjs));
 });
 
-gulp.task('js-minify-dev', function(){
+gulp.task('minify-js-dev', function(){
   //console.logを表示した状態にする
   uglifyOpt.compress.drop_console = false;
   uglifyOpt.compress.drop_debugger = false;
@@ -81,11 +102,16 @@ gulp.task('js-minify-dev', function(){
     .pipe(rename({
       extname: '.min.js'
     }))
-    .pipe(gzip({ gzipOptions: { level: 9 } }))
     .pipe(gulp.dest(path.minjs));
 });
 
-gulp.task('scss-compile', ['admin-scss-compile','contact-scss-compile','socket-sass-compile'] );
+gulp.task('compress-gzip-js', function(){
+  return gulp.src(path.minjsFile)
+    .pipe(gzip({ gzipOptions: { level: 9 }, deleteMode: path.minjs }))
+    .pipe(gulp.dest(path.minjs));
+});
+
+gulp.task('scss-compile', ['admin-scss-compile', 'compress-gzip-admin-css', 'contact-scss-compile', 'compress-gzip-contact-css', 'socket-sass-compile', 'compress-gzip-socket-css'] );
 
 gulp.task('jade-compile', function(){
   return gulp.src(path.jade)
@@ -95,14 +121,30 @@ gulp.task('jade-compile', function(){
     .pipe(gulp.dest(path.outOfJs));
 });
 
-gulp.task('watch', function(){
-  gulp.watch([path.adScss + '**/*.scss'], ['admin-scss-compile']);
-  gulp.watch([path.scss + '**/*.scss'], ['contact-scss-compile']);
-  gulp.watch([path.socketSass + '**/*.sass'], ['socket-sass-compile']);
-  gulp.watch([path.js], ['js-minify-dev']);
-  gulp.watch([path.jade], ['jade-compile']);
+gulp.task('compress-gzip-jade', function(){
+  return gulp.src(path.outOfJsFile)
+    .pipe(gzip({ gzipOptions: { level: 9 } }))
+    .pipe(gulp.dest(path.outOfJs));
 });
 
-gulp.task('dev', ['scss-compile', 'js-minify-dev', 'jade-compile', 'watch']);
+gulp.task('watch', function(){
+  gulp.watch([path.adScss + '**/*.scss'], ['admin-scss-compile', 'compress-gzip-admin-css']);
+  gulp.watch([path.scss + '**/*.scss'], ['contact-scss-compile', 'compress-gzip-contact-css']);
+  gulp.watch([path.socketSass + '**/*.sass'], ['socket-sass-compile', 'compress-gzip-socket-css']);
+  gulp.watch([path.js], ['js-minify-dev', 'compress-gzip-js']);
+  gulp.watch([path.jade], ['jade-compile', 'compress-gzip-jade']);
+});
+
+gulp.task('js-minify', function(){
+  runSequence('minify-js', 'compress-gzip-js');
+});
+
+gulp.task('js-minify-dev', function(){
+  runSequence('minify-js-dev', 'compress-gzip-js');
+});
+
+gulp.task('dev', function(){
+  runSequence('scss-compile', 'js-minify-dev', 'jade-compile', 'compress-gzip-jade', 'watch');
+});
 
 gulp.task('compile-all', ['scss-compile', 'js-minify', 'jade-compile']);
