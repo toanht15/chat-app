93fe0e24254dda6b406658acd4b7ebf164127fee
yuki.henmi <yuki.henmi@medialink-ml.co.jp>
2018-06-22 10:59:39 +0900

    【プロフィール】個人設定表示されるようにする

--- contact_site/app/Controller/PersonalSettingsController.php
+++ contact_site/app/Controller/PersonalSettingsController.php
@@ -75,6 +75,7 @@ class PersonalSettingsController extends AppController {
           // 保存処理
           if ( $this->MUser->save($tmpData, false) ) {
             $this->MUser->commit();
+            $this->userInfo['display_name'] = $tmpData['MUser']['display_name'];
             $this->Session->write('global.userInfo',$this->userInfo);
             $this->Session->read('token');
             $this->set('token', $token);
