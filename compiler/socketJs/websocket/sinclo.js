2b7aad7315c5cd11a122315e84c16887c310333b
masashi.shimizu <masashi.shimizu@medialink-ml.co.jp>
2018-09-25 19:13:24 +0900

    Fix : シナリオ確認時の自由入力エリアが同期されない Fix : 入力待ち状態のイベントが複数登録されるケースがある

--- compiler/socketJs/websocket/sinclo.js
+++ compiler/socketJs/websocket/sinclo.js
@@ -1494,6 +1494,10 @@
               userName = window.sincloInfo.widget.subTitle;
             }
 
+            if(sinclo.scenarioApi._hearing._isConfirming()) {
+              sinclo.scenarioApi._hearing._endInputProcess();
+              sinclo.scenarioApi._hearing._showConfirmMessage();
+            }
             sinclo.chatApi.createMessage(cn, obj.chatMessage, userName, true);
             sinclo.chatApi.scDown();
             return false;
@@ -4936,7 +4940,7 @@
         "1": '.+',
         "2": '[0-9]+',
         "3": "^(([^<>()\\[\\]\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$",
-        "4": '^0[\\d+-]*'
+        "4": '^[-\\d]{10,}$'
       },
       _validChars: {
         "1": '.+',
@@ -5353,7 +5357,6 @@
         var self = sinclo.scenarioApi;
         window.removeEventListener('storage', self._handleStorageUpdateEvent);
       },
-      _scenarioSeqIsUpdated: false,
       _handleStorageUpdateEvent: function(event) {
         var self = sinclo.scenarioApi;
         console.log(event);
@@ -5381,17 +5384,17 @@
                 console.log("<><><><><><><><><><> NOT process %s <><><><><><><><><><>", String(action.actionType));
                 self._handleChatTextArea(self.get(self._lKey.currentScenario).chatTextArea);
               }
-            },100);
+            }, 100);
           } else if(self.isProcessing() && (String(self.get(self._lKey.currentScenario).actionType) === self._actionType.hearing) ) {
             setTimeout(function(){
               console.log('ヒアリング中');
               self._hearing._beginValidInputWatcher();
-            },self._getIntervalTimeSec() * 1000);
-          } else if((oldObj && newObj) && ((oldObj[self._lKey.currentScenario]).actionType === self._actionType.hearing) && ((newObj[self._lKey.currentScenario]).actionType !== self._actionType.hearing)) {
+            }, 100);
+          } else if((oldObj && newObj && oldObj[self._lKey.currentScenario] && newObj[self._lKey.currentScenario]) && ((oldObj[self._lKey.currentScenario]).actionType === self._actionType.hearing) && ((newObj[self._lKey.currentScenario]).actionType !== self._actionType.hearing)) {
             setTimeout(function(){
               console.log('ヒアリング終了時');
               self._hearing._endValidInputWatcher();
-            },self._getIntervalTimeSec() * 1000);
+            }, 100);
           } else if(oldObj && !newObj){
             console.log('シナリオ終了時');
             var beforeTextareaOpened = self.get(self._lKey.beforeTextareaOpened);
@@ -5404,6 +5407,7 @@
           }
         }
       },
+      _scenarioSeqIsUpdated: false,
       _isTheFiestScenaroAndSequence: function () {
         var self = sinclo.scenarioApi;
         var result = false;
@@ -5685,6 +5689,7 @@
       },
       _waitingInput: function (callback) {
         var self = sinclo.scenarioApi;
+        self._unWaitingInput();
         $(document).on(self._events.inputCompleted, function (e, inputVal) {
           callback(inputVal);
         });
@@ -5774,7 +5779,8 @@
         _state: {
           currentSeq: "sh_currentSeq",
           retry: "sh_retry",
-          length: "sh_length"
+          length: "sh_length",
+          confirming: "sh_comfirming"
         },
         _cvType: {
           validOnce: "1",
@@ -5980,12 +5986,11 @@
             self._beginValidInputWatcher();
             self._parent.setPlaceholderMessage(self._parent.getPlaceholderMessage());
             self._parent._showMessage(self._parent.get(self._parent._lKey.currentScenario).actionType, message, self._getCurrentSeq(), self._parent.get(self._parent._lKey.currentScenario).chatTextArea, function () {
+              self._endInputProcess();
               sinclo.chatApi.addKeyDownEventToSendChat();
               self._parent._saveWaitingInputState(true);
               self._parent._waitingInput(function (inputVal) {
-                sinclo.chatApi.removeKeyDownEventToSendChat();
-                self._parent._unWaitingInput();
-                self._endValidInputWatcher();
+                self._endInputProcess();
                 self._parent._handleStoredMessage();
                 if (self._parent._valid(hearing.inputType, inputVal)) {
                   self._parent._saveVariable(hearing.variableName, inputVal);
@@ -6000,6 +6005,12 @@
               });
             });
           });
+        },
+        _endInputProcess: function () {
+          var self = sinclo.scenarioApi._hearing;
+          sinclo.chatApi.removeKeyDownEventToSendChat();
+          self._parent._unWaitingInput();
+          self._endValidInputWatcher();
         },
         _executeConfirm: function () {
           var self = sinclo.scenarioApi._hearing;
@@ -6016,6 +6027,7 @@
             }
           }
           if (self._requireConfirm()) {
+            self._saveConfirmFlg(true);
             self._showConfirmMessage();
           } else {
             if (self._cvIsEnable()) {
@@ -6029,6 +6041,16 @@
               self._parent._process();
             }
           }
+        },
+        _saveConfirmFlg: function (confirming) {
+          var self = sinclo.scenarioApi._hearing;
+          self._parent.set(self._state.confirming, confirming);
+        },
+        _isConfirming: function () {
+          var self = sinclo.scenarioApi._hearing;
+          if (!self || !self._parent) return false;
+          var isConfirming = self._parent.get(self._state.confirming);
+          return typeof(isConfirming) !== null && (isConfirming || isConfirming === 'true');
         },
         _requireConfirm: function () {
           var self = sinclo.scenarioApi._hearing;
@@ -6105,6 +6127,7 @@
                 self._parent._unWaitingInput();
                 self._parent._handleStoredMessage();
                 console.log("inputVal : " + inputVal + " self._parent._lKey.currentScenario.success : " + self._parent.get(self._parent._lKey.currentScenario).success + " self._parent._lKey.currentScenario.cancel : " + self._parent.get(self._parent._lKey.currentScenario).cancel);
+                self._saveConfirmFlg(false);
                 if (inputVal === self._parent.get(self._parent._lKey.currentScenario).success) {
                   self._clearRetryFlg();
                   if (self._cvIsEnable()) {
