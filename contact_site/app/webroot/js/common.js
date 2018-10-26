7bf53da81ba45db97f56838ca199974fd2a3a40e
ryo.hosokawa <ryo.hosokawa@Mlpc016>
2018-10-26 11:34:03 +0900

    add:電話番号を追加ボタンを押した際の挙動telnoタグ挿入からa href="tel:>"挿入に変更

--- contact_site/app/webroot/js/common.js
+++ contact_site/app/webroot/js/common.js
@@ -54,11 +54,11 @@ function addVariable(type,sendMessage,focusPosition){
             break;
         case 2:
           if (sendMessage.value.length == 0) {
-            sendMessage.value += "<telno></telno>";
+            sendMessage.value += '<a href="tel:ここに電話番号を記載">リンクテキスト</a>';
             addPosition = 7;
           }
           else {
-            sendMessage.value = sendMessage.value.substr(0, focusPosition) + "\n" + "<telno></telno>" + sendMessage.value.substr(focusPosition,sendMessage.value.length);
+            sendMessage.value = sendMessage.value.substr(0, focusPosition) + "\n" + '<a href="tel:ここに電話番号を記載">リンクテキスト</a>' + sendMessage.value.substr(focusPosition,sendMessage.value.length);
             addPosition = 8;
           }
           var beforeScrollTop = $(sendMessage).scrollTop();
