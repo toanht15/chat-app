093da6d694f725032b7cc756e35f3a33198a51ef
masashi.shimizu <masashi.shimizu@medialink-ml.co.jp>
2018-12-10 21:04:16 +0900

    Fix : インデックス調整

--- contact_site/app/Config/Migration/1544434005_alter_table_t_history_chat_logs_add_index.php
+++ contact_site/app/Config/Migration/1544434005_alter_table_t_history_chat_logs_add_index.php
@@ -16,15 +16,31 @@ class AlterTableTHistoryChatLogsAddIndex extends CakeMigration {
 	public $migration = array(
 		'up' => array(
 			'create_field' => array(
-				't_history_chat_logs' => array(
+			  't_histories' => array(
+			    'indexes' => array(
+            't_histories_m_companies_id_access_date_id_index' => array('column' => array('m_companies_id', 'access_date', 'id'), 'unique' => 0)
+          )
+        ),
+        't_history_chat_logs' => array(
 					'indexes' => array(
             'idx_t_history_chat_logs_m_companies_id_t_histories_id_created' => array('column' => array('m_companies_id', 't_histories_id', 'created'), 'unique' => 0),
             'idx_t_history_chat_logs_m_companies_id_message_type_created' => array('column' => array('m_companies_id', 'message_type', 'created'), 'unique' => 0),
 						't_history_chat_logs_mcid_mt_nf_c_thid_index' => array('column' => array('m_companies_id', 't_histories_id', 'message_type', 'notice_flg', 'created'), 'unique' => 0),
 						't_history_chat_logs_mcid_thid_mt_c_index' => array('column' => array('m_companies_id', 't_histories_id', 'message_type', 'created'), 'unique' => 0),
+            't_history_chat_logs_mcid_thid_mt_md_index' => array('column' => array('m_companies_id', 't_histories_id', 'message_type', 'message_distinction'), 'unique' => 0),
+            'idx_m_companies_id_message_type_notice_flg' => array('column' => array('m_companies_id', 't_histories_id', 'message_type', 'notice_flg'), 'unique' => 0),
 					),
-				)
-			)
+				),
+        't_history_stay_logs' => array(
+          'indexes' => array(
+            'idx_t_history_chat_logs_message_type_companies_id' => array('column' => array('message_type', 'm_companies_id', 't_histories_id', 'message_distinction', 'created'), 'unique' => 0),
+            'idx_m_companies_id_message_type_notice_flg' => array(),
+          ),
+        ),
+			),
+      'drop_field' => array(
+        't_history_chat_logs' => array('indexes' => array('idx_t_history_chat_logs_message_type_companies_id', 'idx_m_companies_id_message_type_notice_flg')),
+      ),
 		),
 		'down' => array(
 			'drop_field' => array(
