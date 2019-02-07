0498fb8b1bb5b33048c77889751a5586a4fedb0c
masashi.shimizu <masashi.shimizu@medialink-ml.co.jp>
2019-02-07 18:53:43 +0900

    Add : 無料シナリオの設定を希望するかどうかの値を追加

---
+++ contact_site/app/Config/Migration/1549533042_alter_table_m_agreements_add_column_free_scenario_add.php
@@ -0,0 +1,50 @@
+<?php
+class AlterTableMAgreementsAddColumnWebsite extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'alter_table_m_agreements_add_column_website';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(
+			'create_field' => array(
+				'm_agreements' => array(
+					'free_scenario_add' => array('type' => 'integer', 'null' => true, 'default' => '0', 'unsigned' => false, 'after' => 'memo'),
+        )
+      ),
+		),
+		'down' => array(
+			'drop_field' => array(
+				'm_agreements' => array('free_scenario_add')
+      )
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		return true;
+	}
+}
