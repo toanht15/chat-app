675476c56b96da2dc6554a4ab9f9ff8440855677
Hoang The Toan <toan.hoang.the@medialink-ml.co.jp>
2019-04-19 18:32:38 +0900

    hotfix: checkbox json data when send mail

--- compiler/socketJs/websocket/sinclo.js
+++ compiler/socketJs/websocket/sinclo.js
@@ -10514,6 +10514,19 @@
             });
         return resultSet;
       },
+      _handleJsonData: function(data) {
+        for (var key in data) {
+          if (check.isJSON(data[key])) {
+            // handle checkbox json data
+            var checkboxData = JSON.parse(data[key]);
+            if(checkboxData.hasOwnProperty('message')){
+              data[key] = checkboxData.message;
+            }
+          }
+        }
+
+        return data;
+      },
       _getMessage: function() {
         var self = sinclo.scenarioApi;
         return self.get(self._lKey.currentScenario).message;
@@ -11724,6 +11737,8 @@
         _process: function() {
           var self = sinclo.scenarioApi._mail;
           var targetVariables = self._parent._getAllTargetVariables();
+          targetVariables = self._parent._handleJsonData(targetVariables);
+
           var sendData = {
             historyId: sinclo.chatApi.historyId,
             mailType: self._parent.get(
