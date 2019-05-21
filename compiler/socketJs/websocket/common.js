b655f596e9f7fda3e3fbd20e3bfb4aaf0ef704bf
masashi.shimizu <masashi.shimizu@medialink-ml.co.jp>
2019-05-21 18:24:05 +0900

    Update : sinclo-LPのURL変更に伴う対応

--- compiler/socketJs/websocket/common.js
+++ compiler/socketJs/websocket/common.js
@@ -163,7 +163,7 @@ var socket, // socket.io
               check.isset(window.sincloInfo.custom.widget.hideFotter) &&
               window.sincloInfo.custom.widget.hideFotter) ?
               '' :
-              '<p id="fotter">Powered by <a target="sinclo" href="https://sinclo.medialink-ml.co.jp/lp/?utm_medium=web-widget&utm_campaign=widget-referral">sinclo</a></p>';
+              '<p id="fotter">Powered by <a target="sinclo" href="https://sinclo.medialink-ml.co.jp/?utm_medium=web-widget&utm_campaign=widget-referral">sinclo</a></p>';
       // フルプランのPCの場合
       if (window.sincloInfo.contract.chat &&
           (window.sincloInfo.contract.synclo ||
