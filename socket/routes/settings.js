3d53714ef27f5f7bb80c1f794cd924761474b63d
Hoang The Toan <toan.hoang.the@medialink-ml.co.jp>
2019-06-12 15:05:46 +0900

    tiny fix

--- socket/routes/settings.js
+++ socket/routes/settings.js
@@ -137,26 +137,7 @@ router.get("/", function (req, res, next) {
         closeBtnColor = settings.closeBtnColor;
       }
       // 閉じるマウスオーバー
-      var code = settings.mainColor.substr(1), r, g, b;
-      if (code.length === 3) {
-        r = String(code.substr(0, 1)) + String(code.substr(0, 1));
-        g = String(code.substr(1, 1)) + String(code.substr(1, 1));
-        b = String(code.substr(2)) + String(code.substr(2));
-      } else {
-        r = String(code.substr(0, 2));
-        g = String(code.substr(2, 2));
-        b = String(code.substr(4));
-      }
-
-      var balloonR = String(255 - parseInt(r, 16));
-      var balloonG = String(255 - parseInt(g, 16));
-      var balloonB = String(255 - parseInt(b, 16));
-      var codeR = parseInt(balloonR).toString(16);
-      var codeG = parseInt(balloonG).toString(16);
-      var codeB = parseInt(balloonB).toString(16);
-
-      var closeBtnHoverColor = ('#' + codeR + codeG + codeB).toUpperCase();
-
+      var closeBtnHoverColor = '#5432FA';
       if (('closeBtnHoverColor' in settings)) {
         closeBtnHoverColor = settings.closeBtnHoverColor;
       }
