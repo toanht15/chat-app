86b4cc5affcff6156c18d5130bff76aa3a78d4e9
masashi.shimizu <masashi.shimizu@medialink-ml.co.jp>
2019-10-01 17:47:57 +0900

    Revert "fix: iphone input focus error"
    This reverts commit 0210ed7a

--- compiler/socketJs/websocket/sinclo.js
+++ compiler/socketJs/websocket/sinclo.js
@@ -3465,7 +3465,6 @@
       opUser: '',
       opUserName: '',
       continueClickThresholdMSec: 3000,
-      bodyPosition: '',
       messageType: {
         customer: 1,
         company: 2,
@@ -3896,9 +3895,6 @@
                   if (e) e.stopPropagation();
                   sinclo.chatApi.clearPlaceholderMessage();
                   if (check.smartphone()) {
-                    sinclo.chatApi.bodyPosition = document.body.style.position;
-                    document.body.style.position = 'fixed';
-
                     $(document).one('touchstart', function(e) {
                       $(document).trigger('blur');
                     });
@@ -3913,8 +3909,6 @@
                   sinclo.chatApi.setPlaceholderMessage(
                       sinclo.chatApi.getPlaceholderMessage());
                   if (check.smartphone()) {
-                    document.body.style.position = sinclo.chatApi.bodyPosition;
-
                     console.log('スマホ入力フォーカスアウト');
                     setTimeout(function() {
                       sinclo.adjustSpWidgetSize();
@@ -3992,19 +3986,6 @@
                 }
               }
             });
-
-        $(document).on('focus', 'input', function(e) {
-          var position = document.body.style.position;
-          document.body.style.position = 'fixed';
-        })
-        .on('blur', 'input', function(e) {
-          if (position) {
-            document.body.style.position = position;
-          } else {
-            document.body.style.position = '';
-          }
-        });
-
         $('input[name^=\'sinclo-radio\']').each(function(index) {
           if (!sinclo.scenarioApi.isProcessing() &&
               $(this).parents('.sinclo-scenario-msg').length !== 0) {
