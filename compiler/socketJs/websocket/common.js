171d5ac5cfe907f455bd2137ef790455e9b3bc8d
masashi.shimizu <masashi.shimizu@medialink-ml.co.jp>
2019-10-16 16:46:08 +0900

    Fix : スマホでページ遷移 or リロード時に必ず最小化状態で復帰する（2019/10/12対応）

--- compiler/socketJs/websocket/common.js
+++ compiler/socketJs/websocket/common.js
@@ -5050,7 +5050,7 @@ var socket, // socket.io
             }
           } else {
             if ((check.smartphone() && sincloInfo.widget.hasOwnProperty('spAutoOpenFlg') &&
-                Number(sincloInfo.widget.spAutoOpenFlg) === 1) || (dataOpenflg === 'false' && storage.s.get('preWidgetOpened') !== 'true')) {
+                Number(sincloInfo.widget.spAutoOpenFlg) === 1 && dataOpenflg !== 'true') || (dataOpenflg === 'false' && storage.s.get('preWidgetOpened') !== 'true')) {
               console.log('saisyouka');
               //最小化
               if (abridgementType['MinRes']) {
